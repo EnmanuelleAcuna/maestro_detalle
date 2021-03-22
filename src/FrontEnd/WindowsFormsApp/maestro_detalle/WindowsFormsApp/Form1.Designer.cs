85bf92e421e42df303b9e9e8f8c9e4a871fa91c6
Enmanuelle Acuña <emanuelacu@outlook.com>
2021-03-22 00:24:33 -0600

    feat: Adicion de formulario para poder registrar una venta con sus conceptos

---
+++ src/FrontEnd/WindowsFormsApp/maestro_detalle/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvConceptos = new System.Windows.Forms.DataGridView();
+            this.btnAgregarConcepto = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Op = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConceptos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cliente";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(59, 13);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(408, 25);
+            this.txtCliente.TabIndex = 1;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(59, 58);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(118, 25);
+            this.txtCantidad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(454, 58);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(118, 25);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(408, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Precio";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(261, 58);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(118, 25);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre";
+            // 
+            // dgvConceptos
+            // 
+            this.dgvConceptos.AllowUserToAddRows = false;
+            this.dgvConceptos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConceptos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cantidad,
+            this.Nombre,
+            this.Precio,
+            this.Op});
+            this.dgvConceptos.Location = new System.Drawing.Point(12, 148);
+            this.dgvConceptos.Name = "dgvConceptos";
+            this.dgvConceptos.ReadOnly = true;
+            this.dgvConceptos.Size = new System.Drawing.Size(776, 290);
+            this.dgvConceptos.TabIndex = 9;
+            this.dgvConceptos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvConceptos_CellClick);
+            // 
+            // btnAgregarConcepto
+            // 
+            this.btnAgregarConcepto.Location = new System.Drawing.Point(598, 58);
+            this.btnAgregarConcepto.Name = "btnAgregarConcepto";
+            this.btnAgregarConcepto.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarConcepto.TabIndex = 8;
+            this.btnAgregarConcepto.Text = "+";
+            this.btnAgregarConcepto.UseVisualStyleBackColor = true;
+            this.btnAgregarConcepto.Click += new System.EventHandler(this.btnAgregarConcepto_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(692, 488);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            // 
+            // Op
+            // 
+            this.Op.HeaderText = "Op";
+            this.Op.Name = "Op";
+            this.Op.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAgregarConcepto);
+            this.Controls.Add(this.dgvConceptos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConceptos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvConceptos;
+        private System.Windows.Forms.Button btnAgregarConcepto;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewButtonColumn Op;
+    }
+}
+
---
+++ src/FrontEnd/WindowsFormsApp/maestro_detalle/WindowsFormsApp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarConcepto_Click(object sender, EventArgs e)
+        {
+            string cantidad = txtCantidad.Text;
+            string nombre = txtNombre.Text;
+            string precio = txtPrecio.Text;
+
+            dgvConceptos.Rows.Add(new object[]{
+                cantidad, nombre, precio, "Eliminar"
+                });
+
+            txtCantidad.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtPrecio.Text = string.Empty;
+
+            txtCantidad.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Concepto> conceptos = new List<Concepto>();
+                foreach (DataGridViewRow fila in dgvConceptos.Rows)
+                {
+                    Concepto concepto = new Concepto
+                    {
+                        nombre = fila.Cells[1].Value.ToString(),
+                        cantidad = Convert.ToInt32(fila.Cells[0].Value.ToString()),
+                        precio = Convert.ToDecimal(fila.Cells[2].Value.ToString())
+                    };
+                    conceptos.Add(concepto);
+                }
+
+                VentaDB venta = new VentaDB();
+                venta.Add(txtCliente.Text, conceptos);
+                MessageBox.Show("Venta realizada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgvConceptos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex != dgvConceptos.Columns["Op"].Index)
+            {
+                return;
+            }
+
+            dgvConceptos.Rows.RemoveAt(e.RowIndex);
+        }
+    }
+}
