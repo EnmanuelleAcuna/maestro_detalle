0fec584b8b0276e5b0bbc0a79869cc84a02f382c
Enmanuelle Acuña <emanuelacu@outlook.com>
2021-03-22 00:23:27 -0600

    code: Adicion de clase para modelar el objeto concepto

---
+++ src/FrontEnd/WindowsFormsApp/maestro_detalle/WindowsFormsApp/Concepto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp
+{
+    public class Concepto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+    }
+}
